d9448d3ed07e5d78afbc0517f87a39e5d7f6bdee
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2024-05-07 22:29:22 +0200

    ci(gh): [ci] enable deploy (#10)
    * fix(tfm): add autoadjust AWS budgets. rename budgets
    
    * ci: updated .gitignore

--- terraform/budgets.tf
+++ terraform/budgets.tf
@@ -1,7 +1,8 @@
 locals {
+  budget_prefix = "all-accounts"
   budgets = {
     "cost-daily" = {
-      name         = "${local.service_name}-daily-budget"
+      name         = "${local.budget_prefix}-daily-budget"
       budget_type  = "COST"
       limit_amount = var.budget_daily_limit_amount
       limit_unit   = "USD"
@@ -17,7 +18,7 @@ locals {
       }
     }
     "cost-monthly" = {
-      name         = "${local.service_name}-monthly-budget"
+      name         = "${local.budget_prefix}-monthly-budget"
       budget_type  = "COST"
       limit_amount = var.budget_monthly_limit_amount
       limit_unit   = "USD"
@@ -46,6 +47,27 @@ locals {
         }
       }
     }
+    "cost-daily-autoadjusted" = {
+      name         = "${local.budget_prefix}-daily-autoadjusted-budget"
+      budget_type  = "COST"
+      limit_amount = var.budget_daily_limit_amount
+      limit_unit   = "USD"
+      time_unit    = "DAILY"
+      auto_adjust_data = {
+        "forecast" = {
+          auto_adjust_type = "FORECAST"
+        }
+      }
+      notifications = {
+        "actual_100" = {
+          comparison_operator        = "GREATER_THAN"
+          threshold                  = 100
+          threshold_type             = "PERCENTAGE"
+          notification_type          = "ACTUAL"
+          subscriber_email_addresses = [var.budget_notification_email]
+        }
+      }
+    }
   }
 }
 
@@ -66,6 +88,13 @@ resource "aws_budgets_budget" "this" {
       threshold_type             = notification.value.threshold_type
       notification_type          = notification.value.notification_type
       subscriber_email_addresses = notification.value.subscriber_email_addresses
+    }
+  }
+
+  dynamic "auto_adjust_data" {
+    for_each = try(each.value.auto_adjust_data, {})
+    content {
+      auto_adjust_type = auto_adjust_data.value.auto_adjust_type
     }
   }
 }
